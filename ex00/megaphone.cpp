94719c872a873f15edd6d03676638790d25f3ca4
Meri Eskelinen <merituulie@hotmail.fi>
2023-08-26 12:14:35 +0300

    Handle double spaces

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,22 +6,36 @@
 /*   By: meskelin <meskelin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 11:17:10 by meskelin          #+#    #+#             */
-/*   Updated: 2023/08/26 11:34:06 by meskelin         ###   ########.fr       */
+/*   Updated: 2023/08/26 12:13:58 by meskelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+
+
 static void	print_upper(int argc, char **argv)
 {
 	int i = 1;
+	char c = '\0';
 	while (argv[i])
 	{
 		int j = 0;
 		while (argv[i][j])
+		{
+			if (isspace(argv[i][j]) && isspace(c))
+			{
+				j++;
+				continue ;
+			}
+			c = argv[i][j];
 			std::cout << (char)toupper(argv[i][j++]);
-		if (i < (argc - 1))
+		}
+		if (i < (argc - 1) && !isspace(c))
+		{
+			c = ' ';
 			std::cout << ' ';
+		}
 		i++;
 	}
 	std::cout << std::endl;
